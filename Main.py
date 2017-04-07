4323369f3afbdbafb453e050947f5892cc1cd258
Chidi Ekeocha <michael.ekeocha@gmail.com>
2017-04-07 13:28:22 -0400

    Fixed Code completely...prints accuracy as well currently at 95% with K=3

--- Main.py
+++ Main.py
@@ -6,16 +6,13 @@ import accuracy as acc
 
 def main():
     training_set, test_set = data.load_dataset('formatted_file.data')
-
-    print('Train Set: ' + repr(len(training_set)))
-    print('Test Set: ' + repr(len(test_set)))
-
     predictions = []
     k = 3
     for x in range(len(test_set)):
         neighbors = ns.get_neighbors(training_set, test_set[x], k)
         result = rs.get_response(neighbors)
-#        print('> predicted = ' + repr(result) + ', actual = ' + repr(test_set[x][-1]))
+        predictions.append(result[0][0])
+        print('> predicted = ' + repr(result) + ', actual = ' + repr(test_set[x][-1]))
     accuracy = acc.accuracy(test_set, predictions)
     print('Accuracy: ' + repr(accuracy) + '%')
 
--- load_data_set.py
+++ load_data_set.py
@@ -5,19 +5,11 @@ def load_dataset(filename):
     with open(filename, 'rt') as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
-        for x in range(len(dataset) - 1):
+        for x in range(len(dataset)):
             #exclude string in iteration
             for y in range(0, 16):
                 dataset[x][y] = float(dataset[x][y])
-        return dataset[slice(0, 16000)] , dataset[slice(16000, 20000)]
-
-#trainingSet = []
-#testSet = []
-
-#load_dataset('letter-recognition.data', trainingSet, testSet)
-#print len(trainingSet)
-#print len(testSet)
-
+        return dataset[slice(0, 16000)], dataset[slice(16000, 20000)]
 
 
 
